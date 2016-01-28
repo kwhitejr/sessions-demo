8531f7d724cce71aea6a7f6713223078b8b67890
kwhitejr <kwhitejr.edu@gmail.com>
2016-01-27 19:19:50 -1000

    name getter added. bodyparser package added

--- server.js
+++ server.js
@@ -1,4 +1,5 @@
 var express = require('express');
+var bodyParser = require('body-parser');
 
 var PORT = 3000;
 var count = 0;
@@ -11,6 +12,14 @@ app.set('view engine', 'jade');
 app.get('/', function (req, res) {
   count += 1;
   res.send('You are visitor #' + count);
+});
+
+app.get('/name', function (req, res) {
+  res.render('get-name');
+});
+
+app.post('/name', function (req, res) {
+  res.render('greet', {name: req.body.name});
 });
 
 var server = app.listen(PORT, function () {
